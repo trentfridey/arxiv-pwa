5059a053343a8feec84fd364d57052c0acd24f4e
trentfridey <tfri.92@gmail.com>
2020-08-24 21:43:36 -0400

    remove VueKatex

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
-import VueKatex from "vue-katex";
 import "./registerServiceWorker";
 import store from "./store";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -14,7 +13,7 @@ import {
   faExternalLinkAlt
 } from "@fortawesome/free-solid-svg-icons";
 
-Vue.use(VueAxios, axios, VueKatex);
+Vue.use(VueAxios, axios);
 
 library.add(faCaretUp, faCaretDown, faDownload, faExternalLinkAlt);
 
