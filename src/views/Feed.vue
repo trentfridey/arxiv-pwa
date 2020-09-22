f39c47c506822aea73a9952ebad38c459a0fb6a4
trentfridey <tfri.92@gmail.com>
2020-09-21 21:48:34 -0400

    comment out footer

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="container">
+  <!-- TODO: in order to have multiple days worth of
+  papers available, need to have a v-for wrapping the following: -->
     <DateHeader />
     <Vignette v-for="paper in papers" :key="paper.id" :paper="paper" />
-    <InfoFooter />
+    <!-- <InfoFooter /> -->
   </div>
 </template>
 
 <script>
 import Vignette from "@/components/Vignette.vue";
 import DateHeader from "@/components/DateHeader.vue";
-import InfoFooter from "@/components/InfoFooter.vue";
+// import InfoFooter from "@/components/InfoFooter.vue";
 import { mapGetters } from "vuex";
 
 export default {
   name: "Feed",
-  components: { Vignette, DateHeader, InfoFooter },
+  components: { Vignette, DateHeader, /* InfoFooter */ },
   created() {
     this.$store.dispatch("fetchLatestPapers");
   },
